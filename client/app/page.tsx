35a8e9b31d528d7df5d422c1b8b498d8882453b2
andriishepeha <shepeha.andrii@gmail.com>
2026-03-17 17:31:42 +0200

    Initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState<string>();
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/test')
+    // fetch('/api/test')
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        setData(data.slice(0, 20)); // Display only the first 20 characters
+      })
+      .catch(error => console.error('Error fetching test endpoint:', error));
+  }, []);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        Hello {data}
+      </main>
+    </div>
+  );
+}
